5087d8cb4391b16a15bbe05b88b33429f4479ba3
Nailya Ismayilova <nailya.ismail@gmail.com>
2020-04-21 22:45:09 +0400

    test

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-
-
+// global var for storing valuta 
 let strValueFrom = 'RUB';
 let strValueTo = 'USD';
 
 let fromButtons = document.querySelectorAll('.from_button');
 let toButtons = document.querySelectorAll('.to_button');
 
+//setup start valuta (RUB and USD turn lights on)
 fromButtons.forEach((item)=> {
    item.addEventListener('click',changeValuta);
    if (item.innerText == strValueFrom)
@@ -18,19 +18,23 @@ toButtons.forEach((item)=> {
    item.style.background =  "rgba(230,0,0,0.6)";
 })
 
-
-
+/// function that change valuta with button
 function changeValuta (evt) {
    let x = evt.target;
+   let similar = false;
    if(x.className == 'from_button')
    {
+      if(x.innerText != strValueTo)
+      {
+      //turn of selector
       selectValue = document.getElementById('selectValue_from');
       selectValue.style.background = "rgba(0,0,0,0.6)";
+
       let a = document.querySelectorAll('.from_button');
       a.forEach((item)=>{
       item.style.background = "rgba(0,0,0,0.6)"
       strValueFrom = x.innerText;
-
+////// create input event for updateValue event handler function
       let targetLeft = new EventTarget();
       targetLeft.id = 'left_input';
       let event1 = new Event('input', {
@@ -41,16 +45,22 @@ function changeValuta (evt) {
       updateValue(event1);
    });
    }
+   else{
+      alert('similar values');
+      similar = true;
+    }
+   }
 
    if(x.className == 'to_button')
    {
+      if(x.innerText != strValueFrom)
+      {
       selectValue = document.getElementById('selectValue_to');
       selectValue.style.background = "rgba(0,0,0,0.6)";
       let a = document.querySelectorAll('.to_button');
       a.forEach((item)=>{
       item.style.background = "rgba(0,0,0,0.6)";
       strValueTo = x.innerText;
-
       let targetRight = new EventTarget();
       targetRight.id = 'right_input';
       let event2 = new Event('input', {
@@ -61,19 +71,23 @@ function changeValuta (evt) {
       updateValue(event2);
    });
    }
+   else{
+     alert('similar values');
+    similar = true;
+   }
+   }
 
+   if(!similar)
    evt.target.style.background =  "rgba(230,0,0,0.6)";
-
-   
-
+//////// activate and deactivate selector
    if(evt.target.className == 'from_button')
    {
       left_selectorIsActive = false;
    }
-   else{
+   else if (evt.target.className == 'to_button')
+   {
       right_selectorIsActive = false;
    }
-   //selectorIsActive = false; right_selectorIsActive
    console.log( x.innerText);
 
 }
@@ -105,105 +119,84 @@ function changeArrow()
   document.getElementById('value_l').innerText = memoryForExchange;
     
 
- //let leftSelectorMemory;
-      if(!right_selectorIsActive&&!left_selectorIsActive){
-        //let a = document.querySelectorAll('.from_button');
-         a.forEach((item)=>{
-            item.style.background = "rgba(0,0,0,0.6)";
+ 
+if(!right_selectorIsActive&&!left_selectorIsActive){
+   a.forEach((item)=>{
+      item.style.background = "rgba(0,0,0,0.6)";
 
-            if(item.innerText == strValueTo)
-            {
-               item.style.background = "rgba(255,0,0,0.6)";
-               valueFromMemory = strValueFrom;
-               strValueFrom = strValueTo;
-               //leftTextMemory = document.querySelector('.left_input').value;
-               //document.querySelector('.left_input').value = document.querySelector('.right_input').value;
-            }
-         });
-      
-      
-        // let b = document.querySelectorAll('.to_button');
+      if(item.innerText == strValueTo)
+      {
+         item.style.background = "rgba(255,0,0,0.6)";
+         valueFromMemory = strValueFrom;
+         strValueFrom = strValueTo;
+      }
+   });
+
+  
+   b.forEach((item)=>{
+      item.style.background = "rgba(0,0,0,0.6)";
+
+   if(item.innerText == valueFromMemory)
+   {
+      item.style.background = "rgba(255,0,0,0.6)";
+      strValueTo = valueFromMemory;
+   }
+   });
+   return;
+}
+
+   if(right_selectorIsActive&&!left_selectorIsActive){
+
+   b.forEach((item)=>{
+      item.style.background = "rgba(0,0,0,0.6)";
+
+      if(item.innerText == strValueFrom)
+      {
+         item.style.background = "rgba(255,0,0,0.6)";
+         valueFromMemory = strValueTo;
+         strValueTo = strValueFrom;
+      }
+   });
+
+   selector_right.style.background = "rgba(0,0,0,0.6)";
+   a.forEach((item)=>{
+      item.style.background = "rgba(0,0,0,0.6)";
+   });
+
+   selector_left.style.background = "rgba(255,0,0,0.6)";
+   selector_left.style.background = "rgba(255,0,0,0.6)";
+   selector_left.selectedIndex = selector_right.selectedIndex;
+   strValueFrom = valueFromMemory;
+
+      right_selectorIsActive = false;
+      left_selectorIsActive = true;
+      return;
+}
+
+if(!right_selectorIsActive&&left_selectorIsActive){
+
+   a.forEach((item)=>{
+      item.style.background = "rgba(0,0,0,0.6)";
+
+      if(item.innerText == strValueTo)
+      {
+         item.style.background = "rgba(255,0,0,0.6)";
+         valueFromMemory = strValueTo;
+         strValueTo = strValueFrom;
+      }
+   });
+   selector_left.style.background = "rgba(0,0,0,0.6)";
          b.forEach((item)=>{
             item.style.background = "rgba(0,0,0,0.6)";
-
-         if(item.innerText == valueFromMemory)
-         {
-            item.style.background = "rgba(255,0,0,0.6)";
-            strValueTo = valueFromMemory;
-            //document.querySelector('.right_input').value=leftTextMemory;
-         }
          });
+         selector_right.style.background = "rgba(255,0,0,0.6)";
+         selector_right.selectedIndex = selector_left.selectedIndex;
+         strValueFrom = valueFromMemory;
+
+         right_selectorIsActive = true;
+         left_selectorIsActive = false;
          return;
-      }
-
-      if(right_selectorIsActive&&!left_selectorIsActive){
-
-         //let a = document.querySelectorAll('.to_button');
-         b.forEach((item)=>{
-            item.style.background = "rgba(0,0,0,0.6)";
-
-            if(item.innerText == strValueFrom)
-            {
-               item.style.background = "rgba(255,0,0,0.6)";
-               valueFromMemory = strValueTo;
-               strValueTo = strValueFrom;
-               //leftTextMemory = document.querySelector('.left_input').value;
-               //document.querySelector('.left_input').value = document.querySelector('.right_input').value;
-            }
-         });
-
-         selector_right.style.background = "rgba(0,0,0,0.6)";
-         //let b = document.querySelectorAll('.from_button');
-               a.forEach((item)=>{
-                  item.style.background = "rgba(0,0,0,0.6)";
-               });
-
-             selector_left.style.background = "rgba(255,0,0,0.6)";
-               // if (left_selectorIsActive)
-               // leftTextMemory =  strValueFrom;
-               // else
-               selector_left.style.background = "rgba(255,0,0,0.6)";
-               selector_left.selectedIndex = selector_right.selectedIndex;
-               strValueFrom = valueFromMemory;
-
-                right_selectorIsActive = false;
-                left_selectorIsActive = true;
-                return;
-      }
-
-      if(!right_selectorIsActive&&left_selectorIsActive){
-
-         //let a = document.querySelectorAll('.to_button');
-         a.forEach((item)=>{
-            item.style.background = "rgba(0,0,0,0.6)";
-
-            if(item.innerText == strValueTo)
-            {
-               item.style.background = "rgba(255,0,0,0.6)";
-               valueFromMemory = strValueTo;
-               strValueTo = strValueFrom;
-               //leftTextMemory = document.querySelector('.left_input').value;
-               //document.querySelector('.left_input').value = document.querySelector('.right_input').value;
-            }
-         });
-         selector_left.style.background = "rgba(0,0,0,0.6)";
-         //let b = document.querySelectorAll('.from_button');
-               b.forEach((item)=>{
-                  item.style.background = "rgba(0,0,0,0.6)";
-               });
-
-             //selector_left.style.background = "rgba(255,0,0,0.6)";
-               // if (left_selectorIsActive)
-               // leftTextMemory =  strValueFrom;
-               // else
-               selector_right.style.background = "rgba(255,0,0,0.6)";
-               selector_right.selectedIndex = selector_left.selectedIndex;
-               strValueFrom = valueFromMemory;
-
-               right_selectorIsActive = true;
-               left_selectorIsActive = false;
-               return;
-      }
+   }
 
       if (right_selectorIsActive && left_selectorIsActive) {
 
@@ -230,39 +223,17 @@ function changeArrow()
 }
 
 
-// function getCurrentExchange () {
-//   fetch(getNewLink(strValueFrom,strValueTo))
-//   .then(function(response) {
-//     return response.json();
-//   })
-//   .then(function(json) {
-//     console.log(json);
-//     return json;
-//   });
-// return x;
-// }
-// setTimeout(function() {
-//    let element = document.getElementById('loading');
-//    element.classList += "hidden";
-//  }, 2000)
 
 function calculateExchange_left()
 {
-  // let errorBrat = false;
- 
-//   fetch('https://ratesapi.io/documentation/').catch((error) => {
-//       alert('Mistake');
-//       errorBrat = true;
-//    });
+
 timeout().then(currencies => {
    console.log(currencies);
-   var element = document.getElementById('loading');
+   let element = document.getElementById('loading');
    //if(element.classList=='hidden')
    element.style.visibility    = "hidden";
    });
 
-
-   
 
 fetch(getNewLink(strValueFrom,strValueTo))
   .then(function(response) {
@@ -282,30 +253,23 @@ fetch(getNewLink(strValueFrom,strValueTo))
   .then()
   .catch((error) => {
      alert('Mistake');
-  });
-
-
-  
-   
+  });  
 }
 
 function timeout() {
-   var element = document.getElementById('loading');
-   //if(!timeoutSetted)
+   let element = document.getElementById('loading');
    element.style.visibility='visible';
    return new Promise((resolve, reject) => {
       setTimeout(() => resolve(2),300);
     })
  }
 
-
-
 function calculateExchange_right()
 {
 
    timeout().then(currencies => {
       console.log(currencies);
-      var element = document.getElementById('loading');
+      let element = document.getElementById('loading');
       //if(element.classList=='hidden')
       element.style.visibility    = "hidden";
       });
@@ -315,10 +279,8 @@ function calculateExchange_right()
     return response.json();
   })
   .then(function(json) {
-    //console.log(json);
-    //calculator(Object.values(json.rates));
     let rightVal = document.querySelector('.right_input').value;
-    document.querySelector('.left_input').value = Object.values(json.rates)*rightVal;
+    document.querySelector('.left_input').value = Object.values(json.rates)*rightVal  ; //toFixed(3); 
     let valueL = document.getElementById('value_l');
     valueL.innerHTML= '1 '+ strValueFrom +' =' + 1/ Object.values(json.rates) +' '  + strValueTo;
     let valueR = document.getElementById('value_r');
@@ -326,30 +288,15 @@ function calculateExchange_right()
     //return json;
   });
 }
-//calculateExchange();
-// function calculator(rate)
-// {
-//    let leftVal = document.querySelector('.left_input').value;
-    //console.log(calculateExchange_left());
-
-// }
 
 const inputLeft = document.getElementById("left_input");
 inputLeft.addEventListener('input', updateValue);
 const inputRight = document.getElementById('right_input');
 inputRight.addEventListener('input', updateValue);
-
 inputRight.value=1;
-
-
-// let target1 = new EventTarget({
-//    id:'right_input'
-// })
 
 //create fake 'input' event
 let event = new Event('input', {
-   //target:{id:'right_input'},
-   //target:target1,
    bubbles: true,
    cancelable: true,
 
@@ -364,15 +311,14 @@ function updateValue(evt)
 {
   
    let a =  evt.target;
-if( a!=null ){
+   //error handling with if()
+ if( a != null ){
   if(evt.target.value!=null)
-   evt.target.value = evt.target.value.replace(/,/g, '.');  
+   evt.target.value = evt.target.value.replace(/,/g, '.');  /// replace ',' to dot
   if(a.id == 'left_input')
   {
-   //calculateExchange()
-   calculateExchange_left();
 
-  // console.log(a.value);
+   calculateExchange_left();
   }
   if(a.id == 'right_input')
   {
@@ -390,41 +336,20 @@ if( a!=null ){
    }
 }
 
-
+// get a new link 
 function getNewLink (from,to) {
    //link = 'https://api.ratesapi.io/api/latest?base=RUB&symbols=EUR';
    return newlink = 'https://api.ratesapi.io/api/latest?base=' + from + '&symbols=' + to;
 }
 
-// let onclickFired = false;
-// let onchangeFired = false;
-function changeDesign() {
-   // let selectValue = document.getElementById('selectValue_from');
-   // if (onclickFired ==true && onchangeFired == false)
-   // {
-   // selectValue.style.background =  "rgba(0,0,0,0.6)";
-   // onclickFired = false;
-   // }
-   // onclickFired = true;
-}
+
 
 left_selectorIsActive = false;
 right_selectorIsActive = false;
 
+// choose valuta by select
 function chooseValuta(evt) {
-   //onchangeFired = true;
-   //evt.style.background =  "rgba(0,0,0,0.6)";
-   //let selectValue = document.getElementById('selectValue');
-   //console.log( evt.value);
 
-   // let a = document.querySelectorAll('.from_button');
-   // a.forEach((item)=>{
-   // item.style.background = "rgba(0,0,0,0.6)";
-
-   //   if(item.innerText == selectValue.value)
-   //   {
-   //    item.style.background = "rgba(255,0,0,0.6)";
-   //   }
    selectorIsActive = false;
    if(evt.id == 'selectValue_from')
    {
@@ -441,14 +366,6 @@ function chooseValuta(evt) {
    evt.style.background =  "rgba(230,0,0,0.6)";
    left_selectorIsActive = true;
 
-   // let targetRight = new EventTarget();
-   //    targetRight.id = 'right_input';
-   //    let event2 = new Event('input', {
-   //       bubbles: true,
-   //       cancelable: true,
-   //    });
-   //    Object.defineProperty(event2, 'target', {writable: false, value: targetRight});
-   //    updateValue(event2);
 
    let targetLeft = new EventTarget();
    targetLeft.id = 'left_input';
@@ -475,14 +392,7 @@ function chooseValuta(evt) {
    evt.style.background =  "rgba(230,0,0,0.6)";
    right_selectorIsActive = true;
 
-   // let targetLeft = new EventTarget();
-   //    targetLeft.id = 'left_input';
-   //    let event1 = new Event('input', {
-   //       bubbles: true,
-   //       cancelable: true,
-   //    });
-   //    Object.defineProperty(event1, 'target', {writable: false, value: targetLeft});
-   //    updateValue(event1);
+
 
       let targetRight = new EventTarget();
       targetRight.id = 'right_input';
@@ -494,17 +404,4 @@ function chooseValuta(evt) {
       updateValue(event2);
    
    }
-   // if(evt.id == 'to_button')
-   // {
-   //    let a = document.querySelectorAll('.to_button');
-   //    a.forEach((item)=>{
-   //    item.style.background = "rgba(0,0,0,0.6)";
-   //    strValueTo = x.innerText;
-   // });
-    
-
-
-   // evt.target.style.background =  "rgba(230,0,0,0.6)";
-   // console.log( x.innerText);
-
 }
